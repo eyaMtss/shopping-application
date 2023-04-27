17183833a598157da51747bb60c8a0bdbbc18006
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-27 22:18:55 +0100

    lombok

--- product-service/src/main/java/com/eyamattoussi/productservice/model/Product.java
+++ product-service/src/main/java/com/eyamattoussi/productservice/model/Product.java
@@ -1,7 +1,13 @@
 package com.eyamattoussi.productservice.model;
 
+import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(value = "product")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Product {
 }
