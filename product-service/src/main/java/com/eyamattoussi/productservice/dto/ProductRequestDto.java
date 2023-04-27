26268f40ee1d2dce6ac0cd9b58f5eef51e31cc70
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-27 22:28:15 +0100

    dto

--- product-service/src/main/java/com/eyamattoussi/productservice/dto/ProductRequestDto.java
+++ product-service/src/main/java/com/eyamattoussi/productservice/dto/ProductRequestDto.java
@@ -1,4 +1,20 @@
 package com.eyamattoussi.productservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductRequestDto {
+    @Id
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
 }
--- product-service/src/main/java/com/eyamattoussi/productservice/dto/ProductResponseDto.java
+++ product-service/src/main/java/com/eyamattoussi/productservice/dto/ProductResponseDto.java
@@ -1,4 +1,21 @@
 package com.eyamattoussi.productservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponseDto {
+    @Id
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
 }
