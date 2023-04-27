df42c1f729c9dd672ceeeaf9bef8aa1d8d6c245c
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-27 22:49:07 +0100

    exception

---
+++ product-service/src/main/java/com/eyamattoussi/productservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.eyamattoussi.productservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> attributeLengthException(ProductNotFoundException e) {
+        return new ResponseEntity<>("ERROR: " + e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
