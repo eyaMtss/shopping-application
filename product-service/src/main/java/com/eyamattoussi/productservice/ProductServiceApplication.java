e506cfc1824816a39597f0a475de39397ed93f9a
eyaMattoussi <eya.mattoussii@gmail.com>
2023-04-30 16:04:40 +0100

    eureka

--- product-service/src/main/java/com/eyamattoussi/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/eyamattoussi/productservice/ProductServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableMongoRepositories
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
